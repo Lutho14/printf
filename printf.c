07b4cea43e6da4b7b3e43dd1c2350c6c56c9265a
WandiMbu10 <mbuyisaowethu46@gmail.com>
2023-08-22 11:16:49 -0700

    printf

--- printf.c
+++ printf.c
@@ -1,24 +1,55 @@
 #include "main.h"
 #include <stdio.h>
+#include <stdarg.h>
+
+/**
+ * print_char - prints a single charater
+ * @c: the character to printed.
+ * @count: Pointer to the count of character
+ */
+
+void print_char(char c, int *count)
+{
+	putchar(c);
+	(*count)++;
+}
+
+/**
+ * print_string - prints a string
+ * @s: the string to printed
+ * @count: pointer to be count of charecters
+ */
+
+void print_string(char *s, int *count)
+{
+	while (*s)
+	{
+		putchar(*s);
+		s++;
+		(*count)++;
+	}
+}
 
 /**
  * _printf - function that produces output according to a format
  * @format: character string
- * @c:
- * @s:
- * @%:
+ * c:
+ * s:
+ * %:
  * @...: arguments to be formated
  *
  * Return: the number of characters printed
  */
+
 int _printf(const char *format, ...)
 {
 	int count = 0;
 	va_list args;
+	char *p;
 
 	va_start(args, format);
 
-	for (const char *p = format; *p != '\0'; p++)
+	if (char *p = format; *p != '\0'; p++)
 	{
 		if (*p != '%')
 		{
@@ -50,12 +81,12 @@ int _printf(const char *format, ...)
 					break;
 				}
 				case '%':
-					putchar('%');
-					count++;
-					break;
-				default:
-					putchar(*p);
-					count++;
+						putchar('%');
+						count++;
+						break;
+					default:
+						putchar(*p);
+						count++;
 					break;
 			}
 		}

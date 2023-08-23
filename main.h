b1d4d869719cf57849b03f43657717f8bd57e2fd
WandiMbu10 <mbuyisaowethu46@gmail.com>
2023-08-22 17:01:51 -0700

    printf

--- main.h
+++ main.h
@@ -2,9 +2,16 @@
 #define MAIN_H
 
 #include "main.h"
+#include <stdarg.h>
 #include <stdio.h>
-#include <stdlib.h>
+#include <unistd.h>
+
+#define UNUSED(x) (void)(x)
+#define BUFF_SIZE 1024
 
 int _printf(const char *format, ...);
+void print_string(char *s, int *count);
+void print_char(char c, int *count);
+void handle_unknown(char *spc);
 
 #endif
